d3839d2b580edd9261af3575c0e25e38e42c47ab
pdisk <83772019+pdisk@users.noreply.github.com>
2021-05-11 01:08:10 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 from selenium import webdriver
-import os
-
-chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-
+from selenium.webdriver.chrome.options import Options
+options = Options()
+#options.add_argument('--headless')
+#options.add_argument('--disable-gpu')
+driver = webdriver.Chrome(options=options)
 from telegram.ext import *
 from telegram.ext import MessageHandler, Filters
 import time
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 import re
-
 link = ""
+p = False
+pp = True
 fileneemDB = []
+pas = ""
 sorted_links = []
+credDB = []
+cred = ""
 gg = 0
 movie_varient = []
 length = ''
@@ -33,22 +33,22 @@ fileneem = ""
 text = ""
 DB = []
 URLS = []
-kkkk = 0
 t = 1
 k = 1
 u = 1
 w = 0
 n = 1
 j = 1
-h = 1
 cap = ""
 totbutton = ""
 chatid = ""
 length_var = 0
+kkkk = 0
+
 
 
 def start_command(update, context):
-    update.message.reply_text("Sent Me Any Pdisk Post")
+    update.message.reply_text("Sent ME The Any Pdisk Post")
     chatid = update.message.chat_id
     print(chatid)
 def reset_command(update, context):
@@ -78,7 +78,8 @@ def reset_command(update, context):
     global rawname
     global cap
     global j
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    global kkkk
+    kkkk = 0
     gg = 0
     movie_varient = []
     sorted_links = []
@@ -105,7 +106,7 @@ def reset_command(update, context):
     text = ""
     cap = ""
     t = 1
-    
+    k = 1
     u = 1
     w = 0
     n = 1
@@ -223,76 +224,67 @@ def handle_photo(update, context):
     global movie_varient
     global length_var
     global txxt
+    global pp
+    global pas
     URLS = []
     url = []
     chatid = update.message.chat_id
     print(chatid)
 
-    update.message.reply_text("OK Wait Let Me HAndle Things From Now")
-    photo = update.message.photo[-1].file_id
+    if p == True:
+        update.message.reply_text("OK Wait Let Me HAndle Things From Now")
+        photo = update.message.photo[-1].file_id
 
-    text = str(update.message.caption)
-    txxt = text
-    print(text)
-    url = re.findall("(?P<url>https?://[^\s]+)", text)
-    print(url)
-    regrex_pattern = re.compile(pattern="["
-                                        u"\U0001F600-\U0001F64F"  # emoticons
-                                        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-                                        u"\U0001F680-\U0001F6FF"  # transport & map symbols
-                                        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-                                        "]+", flags=re.UNICODE)
-    if re.search(":-", txxt):
-        txxt = txxt.replace(":-", "")
-    elif re.search(":", txxt):
-        txxt = txxt.replace(":", "")
-    elif re.search("", txxt):
-        txxt = txxt.replace(":", "")
-    txxt = txxt.replace("-", "")
-    textd = regrex_pattern.sub(r'', txxt)
+        text = str(update.message.caption)
+        txxt = text
+        print(text)
+        url = re.findall("(?P<url>https?://[^\s]+)", text)
+        print(url)
+        regrex_pattern = re.compile(pattern="["
+                                            u"\U0001F600-\U0001F64F"  # emoticons
+                                            u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                                            u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                                            u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                                            "]+", flags=re.UNICODE)
+        if re.search(":-", txxt):
+            txxt = txxt.replace(":-", "")
+        elif re.search(":", txxt):
+            txxt = txxt.replace(":", "")
+        elif re.search("", txxt):
+            txxt = txxt.replace(":", "")
+        txxt = txxt.replace("-", "")
+        textd = regrex_pattern.sub(r'', txxt)
 
-    movie_var = re.findall(r'\n(.*?)(\n)?http', textd)
+        movie_var = re.findall(r'\n(.*?)(\n)?http', textd)
 
-    movie_varient = [item[0] for item in movie_var]
-    lines = text.split('\n')
-    rawname = lines[0]
-    if re.search("🎬Title", rawname):
-        name = rawname.replace("🎬Title :", "")
+        movie_varient = [item[0] for item in movie_var]
+        lines = text.split('\n')
+        rawname = lines[0]
+        if re.search("🎬Title", rawname):
+            name = rawname.replace("🎬Title :", "")
 
-    elif re.search("🎬 Title", rawname):
-        name = rawname.replace("🎬 Title :", "")
+        elif re.search("🎬 Title", rawname):
+            name = rawname.replace("🎬 Title :", "")
 
-    elif re.search("📼 Title :", rawname):
-        name = rawname.replace("📼 Title :", "")
+        elif re.search("📼 Title :", rawname):
+            name = rawname.replace("📼 Title :", "")
 
-    elif re.search("Title :", rawname):
-        name = rawname.replace("Title :", "")
+        elif re.search("Title :", rawname):
+            name = rawname.replace("Title :", "")
 
-    elif re.search("🎬", rawname):
-        name = rawname.replace("🎬", "")
+        elif re.search("🎬", rawname):
+            name = rawname.replace("🎬", "")
 
-    elif re.search("📼", rawname):
-        name = rawname.replace("📼", "")
+        elif re.search("📼", rawname):
+            name = rawname.replace("📼", "")
 
-    else:
-        name = rawname
+        else:
+            name = rawname
 
-    length = len(url)
-    length_var = len(movie_varient)
-    if re.search("Season", text):
-        print("season Detected")
+        length = len(url)
+        length_var = len(movie_varient)
         URLS = []
-        if n == 1:
-            name = ""
-            n = n + 1
-        update.message.reply_text(
-            "OK Its a Series \n Sent The Name Of The Series With Season Number\nExample:Money Heist S01 ")
 
-        u = 1
-
-        if re.search("Season", text):
-            w = 1
-    else:
         print("Movie Detected")
         u = 1
         if re.search("How", text):
@@ -302,7 +294,15 @@ def handle_photo(update, context):
             print("i = ", i)
             pdurl = url[i]
             nam = str(name.encode('ascii', errors='ignore').decode()) + " - " + movie_varient[i]
+
             webmanager(nam, pdurl, text, photo, context, update)
+    else:
+        update.message.reply_text("You are Not Loged in\n  sent /login yourpassword")
+
+
+
+
+
 
 
 def webmanager(name, url, k, photo, context, update):
@@ -316,23 +316,35 @@ def webmanager(name, url, k, photo, context, update):
     global cap
     global length
     global URLS
+    global p
+    global pp
+    global pas
 
     if t == 1:
+
         driver.get('http://www.pdisk.net/upload?type=url')
         email = driver.find_element_by_xpath('//*[@id="app"]/article/div[1]/div[2]/div[1]/span/input')
         email.send_keys("dhanush544531@gmail.com")
 
         password = driver.find_element_by_xpath('//*[@id="app"]/article/div[1]/div[2]/div[2]/span/input')
-        password.send_keys("dhanush787")
+        password.send_keys(pas)
 
         submit = driver.find_element_by_xpath('//*[@id="app"]/article/div[1]/div[2]/div[3]/button/span')
         submit.click()
         t = t + 1
 
+
     time.sleep(1)
     driver.get('http://www.pdisk.net/upload?type=url')
     if t == 2:
-        nextbutton = driver.find_element_by_xpath('//*[@id="app"]/section/div[2]/div/div[3]')
+        try:
+            nextbutton = driver.find_element_by_xpath('//*[@id="app"]/section/div[2]/div/div[3]')
+            update.message.reply_text("Login Sucessfull")
+
+        except:
+            update.message.reply_text("Invalid Password\nSend /login yourpassword")
+            t =1
+            p = False
         nextbutton.click()
         nextbutton = driver.find_element_by_xpath('//*[@id="app"]/section/div[2]/div/div[3]')
         nextbutton.click()
@@ -350,7 +362,7 @@ def webmanager(name, url, k, photo, context, update):
 
     uploadbutton = driver.find_element_by_xpath('//*[@id="control-hooks"]/div[5]/div/div/div/button/span')
     uploadbutton.click()
-    update.message.reply_text(f"Uploaded {name}")
+    update.message.reply_text(f"Uploaded {name.encode('ascii',errors='ignore').decode()}")
     driver.get('http://www.pdisk.net/home')
     driver.refresh()
     view = driver.find_element_by_xpath(
@@ -360,118 +372,28 @@ def webmanager(name, url, k, photo, context, update):
     print(pdiskurl)
 
     URLS.append(pdiskurl)
+
     if r == 1:
         cap = k.replace(url, pdiskurl)  # url is the urls of the previous text
+        print("k.replcae === ", cap)
+        r = 2
         if u == length:
-
             forwardphoto(update, context, photo, cap)
             movie_button(update, context, photo)
-        r = 2
+
+
     else:
         cap = cap.replace(url, pdiskurl)
+        print("caption Before Forwareding === ", cap)
         print("length= ", length)
-
         if u == length:
+            print(cap)
             forwardphoto(update, context, photo, cap)
-            if re.search("Season", text):
-                buttonmaker(update, context, photo)
-            else:
-                movie_button(update, context, photo)
+            movie_button(update, context, photo)
             print(u, length)
         else:
             print(u, length)
     u = u + 1
-
-
-def handle_Seasons(update, context):
-    global name
-    global w
-    global u
-    global r
-    global length
-    global newname
-    global gg
-    r = 1
-    if w == 1:
-        name = ""
-        name = str(update.message.text)
-
-        w = w + 1
-        if re.search("How", text):
-            length = length - 1
-        w = 2
-    if name != "":
-        if w == 4:
-            gg = int(update.message.text)
-            for i in range(0, length):
-
-                if i + gg <= 9:
-                    newname = name + f"E0{i + gg}"
-                elif i + gg >= 10:
-                    newname = name + f"E{i + gg}"
-
-                pdurl = url[i]
-                webmanager(newname, pdurl, text, photo, context, update)
-        else:
-            update.message.reply_text("Episode")
-            w = 4
-
-
-
-def buttonmaker(update, context, photo):
-    global newname
-    global URLS
-    global j
-    global name
-    global seasoncap
-    global z
-    global totbutton
-    global u
-    u = 1
-    j = 1
-    button = []
-    for i in range(0, len(URLS)):
-        button.append([InlineKeyboardButton(text=f'Episode {i + gg}', url=URLS[i])])
-
-    for l in range(1, 20):
-        if re.search(f"S0{l}", newname):
-            seasonname = name.replace(f"S0{l}", f"Season {l}")
-        elif re.search(f"S{l}", newname):
-            seasonname = newname.replace(f"S{l}", f"Season {l}")
-    context.bot.send_photo(chat_id=chatid, photo=photo, caption=seasonname, reply_markup=InlineKeyboardMarkup(button))
-    if (len(URLS)%2) == 0:
-        x = 1
-        for i in range(0, int(len(URLS)/2)):
-            y = x + 1
-            if i == 0:
-                butt1 = f"Episode {i + gg}={URLS[i]} + Episode {i + gg + 1}={URLS[i + 1]}\n"
-            else:
-                butt1 = butt1 + f"Episode {i + x + gg}={URLS[i + x]} + Episode {i + gg + y}={URLS[i + y]}\n"
-                x = x+1
-    else:
-        ken = len(URLS)+1
-        ken = int(ken/2)
-        x=1
-        for i in range(0, ken):
-            if i == 0:
-                    butt1 = f"Episode {i + gg}={URLS[i]} + Episode {i + gg + 1}={URLS[i + 1]}\n"
-            else:
-                try:
-                    y = x + 1
-                    butt1 = butt1 + f"Episode {i + x + gg}={URLS[i + x]} + Episode {i + gg + y}={URLS[i + y]}\n"
-                    x = x + 1
-                except:
-                    butt1 = butt1 + f"Episode {i + x + gg}={URLS[i + x]}"
-
-
-
-
-
-    update.message.reply_text(butt1)
-
-    r = 1
-
-    name = ""
 
 
 def movie_button(update, context, photo):
@@ -494,29 +416,25 @@ def forwardphoto(update, context, photo, c):
     c = re.sub('@[^\s]+', '@AllMoviesAskForMovies', c)
     context.bot.send_photo(chat_id=chatid, photo=photo, caption=c)
     r = 1
-
     print("U reseted to ", u)
 
     seasoncap = ""
+
 def handle_files(update,context):
     global fileneem
     global h
+    global fileneemDB
 
     time.sleep(7)
     fileneem = str(update.message.document.file_name)
     fileneemDB.append(fileneem)
+
     update.message.reply_text("OK please Wait I Will Upload the files and sent in back to u")
-
-    context.bot.forward_message(chat_id='1676646973', from_chat_id="868213406",message_id=update.effective_message.message_id)
-
-
-
+    context.bot.forward_message(chat_id='1676646973', from_chat_id="868213406", message_id=update.effective_message.message_id)
 def url_command(update,context):
-    url  = str(update.message.text)
+    url = str(update.message.text)
     url = url.replace("/url ", "")
     print(url)
-    global h
-    h = 2
     global t
     global driver
     global DB
@@ -527,69 +445,107 @@ def url_command(update,context):
     global cap
     global length
     global URLS
+    global fileneem
     global fileneemDB
     global kkkk
+    global p
 
     fileneem = fileneemDB[kkkk]
-    if t == 1:
+
+    if p == True:
+        if t == 1:
+            driver.get('http://www.pdisk.net/upload?type=url')
+            email = driver.find_element_by_xpath('//*[@id="app"]/article/div[1]/div[2]/div[1]/span/input')
+            email.send_keys("dhanush544531@gmail.com")
+
+            password = driver.find_element_by_xpath('//*[@id="app"]/article/div[1]/div[2]/div[2]/span/input')
+            password.send_keys(pas)
+
+            submit = driver.find_element_by_xpath('//*[@id="app"]/article/div[1]/div[2]/div[3]/button/span')
+            submit.click()
+            t = t + 1
+
+        time.sleep(1)
         driver.get('http://www.pdisk.net/upload?type=url')
-        email = driver.find_element_by_xpath('//*[@id="app"]/article/div[1]/div[2]/div[1]/span/input')
-        email.send_keys("dhanush544531@gmail.com")
+        if t == 2:
+            try:
+                nextbutton = driver.find_element_by_xpath('//*[@id="app"]/section/div[2]/div/div[3]')
+                context.bot.send_message(chat_id="868213406", text="Login Sucessfull")
 
-        password = driver.find_element_by_xpath('//*[@id="app"]/article/div[1]/div[2]/div[2]/span/input')
-        password.send_keys("dhanush787")
+            except:
+                context.bot.send_message(chat_id="868213406", text="Invalid Password\nSend /login yourpassword")
+                t = 1
+                p = False
+            nextbutton.click()
+            nextbutton = driver.find_element_by_xpath('//*[@id="app"]/section/div[2]/div/div[3]')
+            nextbutton.click()
+            nextbutton = driver.find_element_by_xpath('//*[@id="app"]/section/div[2]/div/div[3]')
+            nextbutton.click()
+            nextbutton = driver.find_element_by_xpath('//*[@id="app"]/section/div[2]/div/div[3]')
+            nextbutton.click()
+            agreebutton = driver.find_element_by_xpath('//*[@id="control-hooks_checkTos"]')
+            agreebutton.click()
+            t = t + 1
 
-        submit = driver.find_element_by_xpath('//*[@id="app"]/article/div[1]/div[2]/div[3]/button/span')
-        submit.click()
-        t = t + 1
 
-    time.sleep(1)
-    driver.get('http://www.pdisk.net/upload?type=url')
-    if t == 2:
-        nextbutton = driver.find_element_by_xpath('//*[@id="app"]/section/div[2]/div/div[3]')
-        nextbutton.click()
-        nextbutton = driver.find_element_by_xpath('//*[@id="app"]/section/div[2]/div/div[3]')
-        nextbutton.click()
-        nextbutton = driver.find_element_by_xpath('//*[@id="app"]/section/div[2]/div/div[3]')
-        nextbutton.click()
-        nextbutton = driver.find_element_by_xpath('//*[@id="app"]/section/div[2]/div/div[3]')
-        nextbutton.click()
-        agreebutton = driver.find_element_by_xpath('//*[@id="control-hooks_checkTos"]')
-        agreebutton.click()
-        t = t + 1
-    upload = driver.find_element_by_xpath('//*[@id="control-hooks_fileUrl"]')
-    upload.send_keys(url)
 
-    filename = driver.find_element_by_xpath('//*[@id="control-hooks_fileTitle"]')
-    filename.send_keys(fileneem)
+        upload = driver.find_element_by_xpath('//*[@id="control-hooks_fileUrl"]')
+        upload.send_keys(url)
 
-    uploadbutton = driver.find_element_by_xpath('//*[@id="control-hooks"]/div[5]/div/div/div/button/span')
-    uploadbutton.click()
-    driver.get('http://www.pdisk.net/home')
-    driver.refresh()
-    view = driver.find_element_by_xpath(
-        '//*[@id="app"]/section/main/section/main/div/div[2]/div/div/div/div/div/div/table/tbody/tr[1]/td[2]')
-    purl = view.text
-    pdiskurl = "http://m.pdisk.net/share-video?videoid={}".format(purl)
-    print(pdiskurl)
-    context.bot.send_message(chat_id = "868213406",text = pdiskurl)
-    kkkk = kkkk+1
+        filename = driver.find_element_by_xpath('//*[@id="control-hooks_fileTitle"]')
+        filename.send_keys(fileneem)
+
+        uploadbutton = driver.find_element_by_xpath('//*[@id="control-hooks"]/div[5]/div/div/div/button/span')
+        uploadbutton.click()
+        driver.get('http://www.pdisk.net/home')
+        driver.refresh()
+        driver.refresh()
+        view = driver.find_element_by_xpath('//*[@id="app"]/section/main/section/main/div/div[2]/div/div/div/div/div/div/table/tbody/tr[1]/td[2]')
+        purl = view.text
+        pdiskurl = "http://m.pdisk.net/share-video?videoid={}".format(purl)
+        print(pdiskurl)
+        context.bot.send_message(chat_id="868213406", text=pdiskurl)
+        kkkk = kkkk + 1
+
+    else:
+        context.bot.send_message(chat_id="868213406", text="You are Not Loged in\nsent /login yourpassword")
+
+def login_command(update,context):
+    global pp
+    global p
+    global pas
+    temp = str(update.message.text)
+    pas = temp.replace("/login ", "")
+    update.message.reply_text("Trying To Login")
+    if pas != "":
+        pp = True
+        p = True
+        update.message.reply_text("Sent The Message Again")
+    else:
+        update.message.reply_text("Invailid Password")
+
+
+
+
+
+
 
 def main():
     global k
     global u
     global r
     print("Bot Has Started")
-    updater = Updater("1745412728:AAGJMHH85G4EP-ngtvn4xTVcegJiTzjFHck", use_context=True)
+    updater = Updater("1664124045:AAGN4IzOmw_PrwSTR9NSx9Ri7Vp4j3-KpIQ", use_context=True)
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("Start", start_command))
     dp.add_handler(CommandHandler("Link", link_command))
     dp.add_handler(CommandHandler("Name", name_command))
-    dp.add_handler(CommandHandler("Url", url_command))
     dp.add_handler(CommandHandler("Reset", reset_command))
-    dp.add_handler(MessageHandler(Filters.document, handle_files))
+    dp.add_handler(CommandHandler("Login", login_command))
+    dp.add_handler(CommandHandler("Url", url_command))
     dp.add_handler(MessageHandler(Filters.photo, handle_photo))
-    dp.add_handler(MessageHandler(Filters.text, handle_Seasons))
+
+    dp.add_handler(MessageHandler(Filters.document, handle_files))
     if k == 1:
         updater.start_polling()
         k = k + 1
